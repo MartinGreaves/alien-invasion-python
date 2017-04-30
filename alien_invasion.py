977a5bd1a5c6c21ee520cb07f3e8b58ae978a103
Henrique Goulart <henriquedsg89@gmail.com>
2017-04-29 22:45:40 -0300

    Add ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,12 +3,15 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 def run_game():
     pygame.init()
     sett = Settings()
     pygame.display.set_caption("Alien Invasion")
     screen = pygame.display.set_mode((sett.screen_width, sett.screen_height))
+
+    ship = Ship(screen)
 
     while True:
         # watch for keyboard and mouse events
@@ -17,6 +20,8 @@ def run_game():
                 sys.exit()
 
         screen.fill(sett.bg_color)
+        ship.blitme()
+
         pygame.display.flip()
 
 run_game()
