3bfbc6f3d3c07c64ebe067b5aac17b9001dc80c1
Henrique Goulart <henriquedsg89@gmail.com>
2017-04-30 00:38:05 -0300

    Add aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,7 @@ import game_functions as gf
 
 from settings import Settings
 from ship import Ship
+from alien import Alien
 
 from pygame.sprite import  Group
 
@@ -19,14 +20,19 @@ def run_game():
     screen = pygame.display.set_mode(screen_size)
 
     ship = Ship(settings, screen)
+    alien = Alien(settings, screen)
 
     bullets = Group()
+    aliens = Group()
+
+    gf.create_fleet(settings, screen, ship, aliens)
 
     while True:
         gf.check_events(settings, screen, ship, bullets)
         ship.update()
-        gf.update_bullets(bullets)
-        gf.update_screen(settings, screen, ship, bullets)
+        gf.update_bullets(aliens, bullets)
+        gf.update_aliens(settings, aliens)
+        gf.update_screen(settings, screen, ship, aliens, bullets)
 
 
 run_game()
